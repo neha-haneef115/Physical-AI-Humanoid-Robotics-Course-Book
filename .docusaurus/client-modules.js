1e24a19805a301e89fd2a6447eb0e6310b793764
Neha Haneef <nehahaneef203@gmail.com>
2025-12-16 21:00:12 +0500

    Replace repo with new folders

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\ShabbirSon\\OneDrive\\Desktop\\Book\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\ShabbirSon\\OneDrive\\Desktop\\Book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\ShabbirSon\\OneDrive\\Desktop\\Book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\ShabbirSon\\OneDrive\\Desktop\\Book\\src\\css\\custom.css"),
+];
